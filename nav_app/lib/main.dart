6da1f107840d74103760c8391d93bb05b0d61963
vidarshanaa15 <159716797+vidarshanaa15@users.noreply.github.com>
2024-02-14 13:20:40 +0530

    OutlineForApp

--- nav_app/lib/main.dart
+++ nav_app/lib/main.dart
@@ -1,71 +1,179 @@
 import 'package:flutter/material.dart';
 
-//this is shini guys and I'm just trying to learn how this works :)
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RouteMaster',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Home'),
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  LoginPageState createState() => LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String _errorMessage = '';
+
+  void _login() {
+
+    // just a basic outline guys...idk how to create database
+    
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // Dummy account database
+    Map<String, String> accounts = {
+      'user1': 'password1',
+      'user2': 'password2',
+    };
+
+    if (accounts.containsKey(username)) {
+      if (accounts[username] == password) {
+        // Successful login
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      } else {
+        // Incorrect password
+        setState(() {
+          _errorMessage = 'Incorrect password.';
+        });
+      }
+    } else {
+      // Account not found
+      setState(() {
+        _errorMessage = 'Account not found.';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Login'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true, 
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
             ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  _errorMessage,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Bus, Train Schedules'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SchedulePage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Nearby Restaurants, Shops'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NearbyPlacesPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SchedulePage extends StatelessWidget {
+  const SchedulePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bus, Train Schedules'),
+      ),
+      body: const Center(
+        child: Text('Display Bus and Train Schedules here'),
+      ),
+    );
+  }
+}
+
+class NearbyPlacesPage extends StatelessWidget {
+  const NearbyPlacesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nearby Restaurants, Shops'),
+      ),
+      body: const Center(
+        child: Text('Display Nearby Restaurants and Shops here'),
+      ),
     );
   }
 }
